673cade820970287fc0540761aae104cdd9c3dd5
haruyq <156767674+haruyq@users.noreply.github.com>
2026-07-20 20:04:27 +0900

    reloadをTrueに

--- src/main.py
+++ src/main.py
@@ -19,4 +19,4 @@ async def handle_api_error(_: Request, exception: Exception):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
