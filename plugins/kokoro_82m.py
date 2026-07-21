c7521ebf9a22202f11c4db0298a1abece2dd43d8
hayabusa255 <fujitato16@outlook.jp>
2026-07-22 08:17:21 +0900

    親リポジトリから外部TTSプラグインを除外

--- plugins/kokoro_82m.py
+++
@@ -1,8 +0,0 @@
-from utils.engine_client import EngineClientPlugin
-
-
-plugin = EngineClientPlugin(
-    name="kokoro_82m",
-    environment_variable="KOKORO_82M_ENGINE_URL",
-    default_base_url="http://127.0.0.1:50101",
-)
--- plugins/melotts_zh.py
+++
@@ -1,8 +0,0 @@
-from utils.engine_client import EngineClientPlugin
-
-
-plugin = EngineClientPlugin(
-    name="melotts_zh",
-    environment_variable="MELOTTS_ZH_ENGINE_URL",
-    default_base_url="http://127.0.0.1:50100",
-)
